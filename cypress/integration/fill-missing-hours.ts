8db17126f4a435562af224d454731f7c07880e56
idanRiski <idan.elitzur@riskified.com>
2022-02-17 00:00:31 +0200

    feat: check the user role

--- cypress/integration/fill-missing-hours.ts
+++ cypress/integration/fill-missing-hours.ts
@@ -3,6 +3,7 @@ import * as users from '../support/utils/users';
 
 let i: number = 0;
 let rows: any = 0;
+let tabs: any = 0;
 
 describe('Fill missing hours', () => {
 
@@ -16,32 +17,38 @@ describe('Fill missing hours', () => {
     });
 
     it('Select missing hours in Calendar page', async  () => {
+        cy.get("body").then($body => {
+            if ($body.find(loc.landing.missingHourRow).length > 0) {
+                // Check if the user is with manager role
+                cy.get(loc.landing.tabs).should('be.visible').then((tab) => {
+                    tabs = tab.length;
+                    console.log("tabs: " + tabs);
+                    if (tabs > 1) {
+                        cy.get(loc.landing.tabs).eq(1).click();
+                    } else {
+                        return;
+                    }
+                });
 
-        cy.get(loc.landing.missingHourRow).should('be.visible').then((row) => {
-            rows = row.length;
-            console.log("rows: " + rows);
-
-        if (rows > 0) {
-            for (i = 0; i < rows; i++) {
-                cy.get(loc.landing.missingHourBtn).eq(0).should('be.visible').click();
-                cy.get(loc.calendar.calendar).should('be.visible');
-                cy.frameLoaded(loc.calendar.iframe);
-                cy.iframe().find(loc.calendar.tableDiv).should('be.visible');
-                cy.iframe().find(loc.calendar.tableRaw).should('be.visible')
-                    .then(($el) => {
-                        cy.wrap($el).eq(1).find(loc.calendar.tableRow).eq(0).type('{movetostart}' + Cypress.env('startHour'));
-                        cy.wrap($el).eq(1).find(loc.calendar.tableRow).eq(1).type('{movetostart}' + Cypress.env('endHour'));
-                    });
-                cy.server();
-                cy.route('GET', '/EmployeeErrorHandling.aspx').as('route1');
-                cy.iframe().find(loc.calendar.saveButton).contains('שמור וסגור').should('be.visible').dblclick();
-                cy.wait(1000);
-                cy.reload();
+                for (i = 0; i < rows; i++) {
+                    cy.get(loc.landing.missingHourBtn).eq(0).should('be.visible').click();
+                    cy.get(loc.calendar.calendar).should('be.visible');
+                    cy.frameLoaded(loc.calendar.iframe);
+                    cy.iframe().find(loc.calendar.tableDiv).should('be.visible');
+                    cy.iframe().find(loc.calendar.tableRaw).should('be.visible')
+                        .then(($el) => {
+                            cy.wrap($el).eq(1).find(loc.calendar.tableRow).eq(0).type('{movetostart}' + Cypress.env('startHour'));
+                            cy.wrap($el).eq(1).find(loc.calendar.tableRow).eq(1).type('{movetostart}' + Cypress.env('endHour'));
+                        });
+                    cy.intercept('GET', '/EmployeeErrorHandling.aspx').as('route1');
+                    cy.iframe().find(loc.calendar.saveButton).contains('שמור וסגור').should('be.visible').dblclick();
+                    cy.wait('route1');
+                    cy.reload();
+                }
+            } else {
+                cy.log('No new hours to save!');
+                console.log('No new hours to save!');
             }
-        } else {
-            cy.log('No new hours to save!');
-            console.log('No new hours to save!');
-        }
         });
     });
 });
--- cypress/support/locators/landing-page.ts
+++ cypress/support/locators/landing-page.ts
@@ -1,3 +1,4 @@
 export const main = 'h-main-layout';
 export const missingHourBtn = 'button.secondary.float-right.ml.hbutton2.btn';
 export const missingHourRow = 'h-home-tasks div.row.row-card-body.task-row';
+export const tabs = 'div.pr.d-flex.align-items-center.nav-link.active';
